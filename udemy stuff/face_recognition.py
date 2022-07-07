ef6dfda8d9d972acf8d51317db9f30a32cf15aab
Hesam Mohebi <mohebi.hesam@gmail.com>
2022-07-07 16:59:45 +0430

    face detection

--- udemy stuff/face_recognition.py
+++ udemy stuff/face_recognition.py
@@ -1,1 +1,14 @@
+#importing library
 import cv2
+
+# loading the cascades
+# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+
+# defining function
+
+# def detect(gray, frame):
+#     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
